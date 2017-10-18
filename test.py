d31b4e85c5d385c45a0b12829b0eb6e1cfee27e4
chanwang-sf <32891632+chanwang-sf@users.noreply.github.com>
2017-10-17 22:17:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,18 +3,12 @@ import pandas as pd
 path = r"C:\Users\cw83\Desktop\水彩颜料.xlsx"
 df = pd.read_excel(path, sheetname='data')
 print (df)
-###if lname already populated, leave it alone; otherwise combine fname and lname then select 1: as lname
-# df1 = df.lname1.isnull()
-# print (df1)
-# print ((df.fname1.fillna('') + ' ' + df.lname1.fillna('')).apply(lambda x: str(x).split()[0]))
-# print (df['lname1'][df.lname1.isnull()==True])
 
+###if lname already populated, leave it alone; otherwise combine fname and lname then select 1: as lname
 fullname = df.fname1.fillna('') + [' '] + df.lname1.fillna('')
 print (fullname)
 df['fname1'][df.lname1.isnull()] = fullname.apply(lambda x: str(x).split()[0])
 df['lname1'][df.lname1.isnull()] = fullname.apply(lambda x: x[len(str(x).split()[0]):])
-
-# [df.fname1 + [''] + df.lname1][df.lname1.isnull()] = [df.fname1 + [''] + df.lname1]
 print (df)
 
 ### to get mname from lname
